1669e5fa405db0fc96854596482451276446577e
Roman Mzh <akentik@gmail.com>
2019-10-31 18:08:08 +0300

    deployment setup

---
+++ ecosystem.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  apps : [{
+    name: 'ggrecha',
+    script: 'dist/index.js',
+
+    // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
+    instances: 1,
+    autorestart: true,
+    watch: false,
+    max_memory_restart: '1G',
+    env: {
+      NODE_ENV: 'development'
+    },
+    env_production: {
+      NODE_ENV: 'production'
+    }
+  }],
+
+  deploy : {
+    production : {
+      user : 'da',
+      host : '178.62.200.15',
+      ref  : 'origin/master',
+      repo : 'git@github.com:Roman-Mzh/ggrecha.git',
+      path : '/home/da/apps/ggrecha',
+      env  : {
+        NODE_ENV  : 'production'
+      }
+    }
+  }
+};
