efe05bec8801840a0bd0bed65ac1d98ed0019de3
Roman Mzh <akentik@gmail.com>
2019-11-01 15:02:16 +0300

    .

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ const env = process.env.NODE_ENV || 'development';
 var ggrecha;
 
 if (env === 'development') {
-  ggrecha = require('./src/index').default(appLog);
+  ggrecha = require('./src/index').default();
 } else {
-  ggrecha = require('./dist/index').default(appLog);
+  ggrecha = require('./dist/index').default);
 }
 
 console.log(ggrecha)
